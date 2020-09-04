d20bfdf2729fbf99f16a7e980e7c159c3edb81d9
Esteban-Bravo <estebanbra09@gmail.com>
2020-09-04 01:33:12 -0300

    Se agrego el login junto con sus rutas y controladores y se elimo archivos que aun no se utilizaban como user

--- Site/app.js
+++ Site/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var registerRouter = require('./routes/register');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/register', registerRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Site/controllers/loginController.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+
+module.exports ={ 
+    home:function(req,res) {
+        res.render('login',{
+            title:'registrar usuario'
+        })
+    }
+}
